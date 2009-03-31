71f4dcd7e0bb5f7ac962fa07169396f5e45e21ee
Stefan Pintilie <spintilie@sri.utoronto.ca>
2009-03-31 11:36:34 -0400

    BugID: Analysis:Added the QVTKWidget to the 3D view.
    Resolution:
    
    Reviewed by:

--- rtUI/rtMainWindow.cpp
+++ rtUI/rtMainWindow.cpp
@@ -1,8 +1,36 @@
 #include <QtGui>
+#include <QApplication>
 #include "rtMainWindow.h"
 
 rtMainWindow::rtMainWindow(QWidget *parent, Qt::WindowFlags flags) {
   setupUi(this);
 
-  
+  m_render3DVTKWidget = new QVTKWidget(this->frame3DRender);
+  m_render3DLayout = new QHBoxLayout();
+
+  m_inter3D = m_render3DVTKWidget->GetInteractor();
+  m_renWin3D = m_inter3D->GetRenderWindow();
+
+  m_render3DLayout->addWidget(m_render3DVTKWidget);
+  this->frame3DRender->setLayout(m_render3DLayout);
+
+  // The GUI must be stretched more than the tabs. 
+  mainRLSplitter->setStretchFactor(0, 1);
+  mainRLSplitter->setStretchFactor(1, 25);
+
+  // The 3D view must be larger than the 2D view. 
+  mainUDSplitter->setStretchFactor(0, 5);
+  mainUDSplitter->setStretchFactor(1, 1);
+
+  connectSignals();
+}
+
+rtMainWindow::~rtMainWindow() {
+  if (m_render3DVTKWidget) delete m_render3DVTKWidget;
+  if (m_render3DLayout) delete m_render3DLayout;
+}
+
+void rtMainWindow::connectSignals() {
+
+  connect(actionQuit, SIGNAL(triggered()), qApp, SLOT(quit()));
 }
--- rtUI/rtMainWindow.h
+++ rtUI/rtMainWindow.h
@@ -1,7 +1,11 @@
 #ifndef RT_MAIN_WINDOW_H
 #define RT_MAIN_WINDOW_H
 
+#include <QHBoxLayout>
 #include "ui_rtMainWindow.h"
+#include <QVTKWidget.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderWindow.h>
 
 class rtMainWindow : public QMainWindow, private Ui::rtMainWindowUI
 {
@@ -9,7 +13,15 @@ class rtMainWindow : public QMainWindow, private Ui::rtMainWindowUI
  
  public:
   rtMainWindow(QWidget *parent = 0, Qt::WindowFlags flags = 0);
+  ~rtMainWindow();
 
+ protected:
+  QVTKWidget *m_render3DVTKWidget;
+  QHBoxLayout *m_render3DLayout;
+  vtkRenderWindowInteractor *m_inter3D;
+  vtkRenderWindow *m_renWin3D;
+
+  void connectSignals();
 };
 
 
