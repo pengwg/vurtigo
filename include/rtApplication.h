c17a5eb545a74b526530bfa10e11006193ed1916
Stefan Pintilie <spintilie.sri.utoronto.ca>
2010-02-09 16:05:28 -0500

    Created an application class for startup

---
+++ include/rtApplication.h
@@ -0,0 +1,53 @@
+/*******************************************************************************
+    Vurtigo: Visualization software for interventional applications in medicine
+    Copyright (C) 2009 Sunnybrook Health Sciences Centre
+
+    This file is part of Vurtigo.
+
+    Vurtigo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+#ifndef RTAPPLICATION_H
+#define RTAPPLICATION_H
+
+#include <QTextStream>
+
+class rtMainWindow;
+
+//! The main application class [singleton]
+/*!
+  */
+class rtApplication
+{
+public:
+  ~rtApplication();
+
+  static rtApplication& instance();
+
+  void displayCopyright();
+  void initApp();
+
+  rtMainWindow* const getMainWinHandle() { return m_mainWindow; }
+
+protected:
+  QTextStream* m_stream;
+  rtMainWindow* m_mainWindow;
+
+
+private:
+  rtApplication();
+  rtApplication(const rtApplication&);
+  rtApplication& operator=(const rtApplication&);
+};
+
+#endif // RTAPPLICATION_H
---
+++ rtControl/rtApplication.cpp
@@ -0,0 +1,56 @@
+/*******************************************************************************
+    Vurtigo: Visualization software for interventional applications in medicine
+    Copyright (C) 2009 Sunnybrook Health Sciences Centre
+
+    This file is part of Vurtigo.
+
+    Vurtigo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+#include "rtApplication.h"
+#include "rtMessage.h"
+#include "rtMainWindow.h"
+
+
+#include <QTextStream>
+
+rtApplication::rtApplication() {
+  // This defaults to standard out.
+  m_stream = new QTextStream(stdout, QIODevice::WriteOnly);
+  m_mainWindow = NULL;
+}
+
+rtApplication::~rtApplication() {
+  if(m_stream) delete m_stream;
+  if(m_mainWindow) delete m_mainWindow;
+}
+
+rtApplication& rtApplication::instance() {
+  static rtApplication handle;
+  return handle;
+}
+
+void rtApplication::displayCopyright() {
+  (*m_stream) << "Vurtigo Copyright (C) 2009 Sunnybrook Health Sciences Centre. ";
+  (*m_stream) << "This program comes with ABSOLUTELY NO WARRANTY; ";
+  (*m_stream) << "This is free software, and you are welcome to redistribute it under certain conditions; ";
+  (*m_stream) << "See COPYING and COPYING.LESSER for details.\n\n";
+  m_stream->flush();
+}
+
+void rtApplication::initApp() {
+
+  // setup the messaging
+  rtMessage::instance().setStream(m_stream);
+  m_mainWindow = new rtMainWindow();
+}
