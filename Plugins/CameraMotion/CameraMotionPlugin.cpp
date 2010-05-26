367ae3dea11a920dba27faa6fc89f57de071ed69
Stefan Pintilie <spintilie@sri.utoronto.ca>
2010-05-26 13:27:54 -0400

    Added functionality to the camera motion plug-in. BugID:716 Analysis:Created the simple version of the camera motion plug-in.
    Resolution:
    
    Reviewed by:

--- Plugins/CameraMotion/CameraMotionPlugin.cpp
+++ Plugins/CameraMotion/CameraMotionPlugin.cpp
@@ -18,26 +18,130 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 #include "CameraMotionPlugin.h"
-#include <iostream>
+#include "rtApplication.h"
+#include "rtMainWindow.h"
+#include "rtCameraControl.h"
+#include "rtBaseHandle.h"
+
+#include "rt3dVolumeDataObject.h"
+
+#include <vtkImageSinusoidSource.h>
 
 void CameraMotionPlugin::objectModified(int objID) {
-
+  // No objects are watched by this plugin.
 }
 
 bool CameraMotionPlugin::init() {
 
+  // First create the GUI
+  // Create the main widget (From DataInterface).
+  // This parameter is NULL if the plugin does not use a widget.
+  m_mainWidget = new QWidget();
+
+  // Create the layout.
+  m_mainPluginGuiLayout = new QHBoxLayout();
+
+  // Create the widgets to go into the layout.
+  m_viewAngleLabel = new QLabel();
+  m_viewAngleAmtLabel = new QLabel();
+  m_viewAngleSlider = new QSlider();
+
+  // Add the widgets to the layout
+  m_mainPluginGuiLayout->addWidget(m_viewAngleLabel);
+  m_mainPluginGuiLayout->addWidget(m_viewAngleSlider);
+  m_mainPluginGuiLayout->addWidget(m_viewAngleAmtLabel);
+
+  // Set the propeties for those widgets.
+  m_viewAngleLabel->setText("View Angle: ");
+  m_viewAngleAmtLabel->setText("30");
+  m_viewAngleSlider->setMinimum(1);
+  m_viewAngleSlider->setMaximum(179);
+  m_viewAngleSlider->setValue(30);
+  m_viewAngleSlider->setOrientation(Qt::Horizontal);
+
+  // Make the horizontal at least 180 pixels.
+  m_viewAngleSlider->setMinimumSize(180, 20);
+
+  // Set the layout containing the custom widgets.
+  m_mainWidget->setLayout(m_mainPluginGuiLayout);
+
+  // Connect the signal to the slot.
+  connect(m_viewAngleSlider, SIGNAL(valueChanged(int)), this, SLOT(viewAngleChanged(int)));
+
+  // Create a simple Vurtigo test object.
+  createTestObject();
+
+  // Return true to show that the plugin has been created as required.
   return true;
 }
 
 void CameraMotionPlugin::cleanup() {
-
+  // Note that there is no cleanup of widgets here.
+  // In Qt when a widget is added to a layout or a layout is added to a widget the parent takes ownership of the child and is will delete it.
 }
 
 void CameraMotionPlugin::update(){
-
+  // No regular updates needed for this plugin
 }
 
 void CameraMotionPlugin::point3DSelected(double px, double py, double pz, int intensity) {
+  // Plugin does not watch for point selection in the 3D view.
+}
+
+void CameraMotionPlugin::viewAngleChanged(int angle) {
+
+  // Change the text in the GUI to inform the user that a change has taken place.
+  m_viewAngleAmtLabel->setText(QString::number(angle));
+
+  // Get a pointer to Vurtigo's camera control
+  rtCameraControl *camera;
+  camera = rtApplication::instance().getMainWinHandle()->getCameraControl();
+
+  // Give the camera control the new view angle.
+  camera->setViewAngle(static_cast<double>(angle));
+}
+
+
+void CameraMotionPlugin::createTestObject() {
+  // Ask the base handle (Vurtigo) to create a rtConstants::OT_3DObject type object.
+  // This function will return the unique ID for the new object.
+  m_smallVol = rtBaseHandle::instance().requestNewObject(rtConstants::OT_3DObject, "Small Test Volume");
+
+  // Check that the object was actually created.
+  // If an object creation fails then the id is negative.
+  if (m_smallVol >= 0) {
+
+    // Get the object with the unique ID
+    rt3DVolumeDataObject* ptObj = static_cast<rt3DVolumeDataObject*>(rtBaseHandle::instance().getObjectWithID(m_smallVol));
+
+    // Check that the object exists.
+    if (ptObj) {
+
+      // Create a source of input data.
+      vtkImageSinusoidSource* sinSrc = vtkImageSinusoidSource::New();
+      sinSrc->SetWholeExtent(1,32, 1, 32, 1, 32);
+      sinSrc->SetDirection(1, 2, 3);
+      sinSrc->SetPeriod(5);
+      sinSrc->SetPhase(1);
+      sinSrc->SetAmplitude(15);
+      sinSrc->Update();
+
+      // Lock the object before doing modifications
+      ptObj->lock();
+
+      // Copy the image data over.
+      ptObj->copyNewImageData(sinSrc->GetOutput());
+
+      // Reset the modified time.
+      ptObj->Modified();
+
+      // Unlock the object after modifications are finished.
+      ptObj->unlock();
+
+      // Delete the source as it has already been copied.
+      sinSrc->Delete();
+    }
+  }
 
 }
 
--- Plugins/CameraMotion/CameraMotionPlugin.h
+++ Plugins/CameraMotion/CameraMotionPlugin.h
@@ -22,19 +22,80 @@
 
 #include "rtPluginInterface.h"
 
+// Qt includes
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QSlider>
+
 //! Plugin to control certain movements of the camera.
 class CameraMotionPlugin : public QObject, public DataInterface {
 
+  // Need to use Q_OBJECT so that this class can use signals and slots.
   Q_OBJECT
+  // This class implements the 'DataInterface'.
   Q_INTERFACES(DataInterface)
 
  public:
+  //! This function is called when an object in the base is modified.
+  /*!
+    The plugin can register to have certain objects as part of a waatch list and be alerted when they change.
+    \sa DataInterface::addObjectToWatchList()
+   */
   void objectModified(int objID);
+
+  //! Function called when the plugin is first created.
+  /*!
+    All of the plugin setup commands should go in here.
+    */
   bool init();
+
+  //! Function called just before the plugin is deleted.
+  /*!
+    All of the plugin cleanup work is done here.
+    */
   void cleanup();
+
+  //! Plugin can register to receive update calls every so often
+  /*!
+    Called by the base at regular time intervals.
+    \sa DataInterface::setUpdateTime()
+    */
   void update();
+
+  //! Plugins can watch of clicks in the 3D window.
+  /*!
+    Plugin must register with the rtBaseHandle first.
+    \sa rtBaseHandle::watchClick()
+    */
   void point3DSelected(double px, double py, double pz, int intensity);
 
+ public slots:
+
+  //! Slot that is called when the view angle is changed with the slider.
+  void viewAngleChanged(int);
+
+ protected:
+
+  //! Create a Simple Vurtigo test object.
+  void createTestObject();
+
+  //! Object ID for the small test object.
+  int m_smallVol;
+
+  ////////////
+  // Gui Elements
+  ////////////
+  //! Layout for the plugin GUI
+  QHBoxLayout *m_mainPluginGuiLayout;
+
+  //! Generic Qt Label to inform the user about the purpose of the slider
+  QLabel *m_viewAngleLabel;
+
+  //! Qt Labal to show the acurrent view angle value
+  QLabel *m_viewAngleAmtLabel;
+
+  //! Slider so the user can adjust the view angle.
+  QSlider *m_viewAngleSlider;
  private:
 
 };
